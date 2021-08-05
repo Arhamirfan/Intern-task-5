c9beef737a4a7f5d65ac7d8d10623b1e78b55485
Arhamirfan <arham.irfan4@gmail.com>
2021-08-06 02:50:14 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# intern_task_5
+
+A new Flutter project with use of stateful and on star icon click, icon change and count increase
---
+++ android/app/src/main/java/com/slayer/intern_task_5/MainActivity.java
@@ -0,0 +1,6 @@
+package com.slayer.intern_task_5;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
